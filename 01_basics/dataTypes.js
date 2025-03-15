d1fed9f81871e768a75c83d1da66ce667e622882
Mayank7776 <111176265+Mayank7776@users.noreply.github.com>
2025-03-15 05:40:20 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer Version of JS
+
+// alert("hello") // we are using nodeJs not browser 
+
+// string => ""
+let name = "Mayank"
+
+// number => 2 TO POWER 53
+let age = 21
+
+// Boolean = TRUE/FALSE
+let isLockedIn = true
+
+// bigInt 
+// Null => standalone Value, also null is a object
+// Undefined => not assigned any value, type of undefined is undefined
+// symbol => unique
+
+// object
+
+// console.log(typeof undefined)
+

7342c2a6025529a82b4b16e5d329fdfb88c42edf
Mayank7776 <111176265+Mayank7776@users.noreply.github.com>
2025-03-15 08:13:43 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//  33 easily converted to number 
+// 33anc => NaN(not a number) but type is number
+
+let lockedIn = ""
+let BooleanIn = Boolean(lockedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "mayank" => true
+
+// console.log(BooleanIn)
+
+let stringval = 43
+
+let stringIn = String(stringval)
+
+console.log(typeof stringIn)
+console.log(stringIn)
+
+

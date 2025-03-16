fdd76e17c348a7831565af69588fcfb226a806a0
Mayank7776 <111176265+Mayank7776@users.noreply.github.com>
2025-03-16 04:51:27 +0000

    math and num

---
+++ 01_basics/numAndMath.js
@@ -0,0 +1,8 @@
+console.log(Math.random())
+console.log(Math.random() * 10 ) 
+console.log(Math.floor((Math.random() * 10 ) + 1)) 
+
+let max = 6
+let min = 1
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)

246eb67141414023f0304542bd1d6489c3bf2b10
Mayank7776 <111176265+Mayank7776@users.noreply.github.com>
2025-03-20 08:34:08 +0000

    loops

---
+++ 01_basics/iteration.js
@@ -0,0 +1,68 @@
+// for
+// for(let i = 0; i < 10; ++i){
+//     console.log(i)
+// }
+// shift + alt + down key: to copy and paste to next line.
+
+// ctrl + d : for multi cursor on same name value
+
+// break : when the value is detected the loop will be distroyed from current iteration.
+
+// continue : only the iteration is skip for one time.
+
+// while loop
+
+// let i = 0
+
+// while(i < 10){
+//     console.log(i)
+//     i++
+// }
+
+// do while  
+
+// let i = 1
+
+// do{
+//     i++
+//     console.log(i)
+// }while(i<10)
+
+
+// Array specific loop
+//  for of loop
+
+// const arr = [1,2,3,4,5]
+
+// for (const i of arr) {
+//     console.log(i)
+// }
+
+
+//  Map : It allow only unique values.
+
+// const map = new Map()
+// map.set('IN','India')
+// map.set("Eu", "Europe")
+
+// console.log(map)
+
+// for in loop : get only keys of object or array.
+
+// const obj = {
+//     In : "India",
+//     EU : "Europe"
+// }
+
+// for(const key in obj){
+//    console.log(`${key} shortcut is for ${obj[key]}`)
+// }
+
+
+// for each loop
+
+const arr = [1,2,3,4,5,6]
+
+arr.forEach(i => {
+ console.log(i)   
+});
